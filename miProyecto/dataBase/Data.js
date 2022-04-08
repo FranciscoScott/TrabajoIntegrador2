06991d3e1c78992d99b538c0d636b51d80820b9d
FranciscoScott <fscott@udesa.edu.ar>
2022-04-07 21:54:51 -0300

    agregue las imagenes a public, hice el boton de enviar formularios y ordene un poco los archivos

--- miProyecto/dataBase/Data.js
+++ miProyecto/dataBase/Data.js
@@ -1,78 +1,76 @@
-const zapatillas ={
-users :[
-    {
-        usuarios:"",
+const zapatillas = {
+    users: [{
+        usuarios: "",
 
-    }
-],
-    shoes:[
-        {
-    marca:"nike",
-    modelo: "Jordan 1",
-    color:"rojo",
-    imagen:"https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901", 
-    descripcion: "", 
-},
-    {
-    marca:"nike",
-    modelo: "Jordan 1",
-    color:"blanco",
-    imagen:"https://http2.mlstatic.com/D_NQ_NP_848406-MLA48841489272_012022-W.jpg",
-    descripcion: "", 
-    },
-    {
-        marca:"nike",
-        modelo: "Jordan 1",
-        color:"university",
-        imagen:"https://images.stockx.com/images/Air-Jordan-1-Retro-High-White-University-Blue-Black-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1611777406",
-        descripcion: "", 
-        },
-
-    {
-        marca:"nike",
-        modelo: "Jordan 1",
-        color:"blue",
-        imagen: "https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Dark-Marina-Blue-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1642023273",
-        descripcion: "", 
-        },
-        {
-            marca:"nike",
+    }],
+    shoes: [{
+            marca: "nike",
             modelo: "Jordan 1",
-            color:"mocha",
-            imagen: "https://images.stockx.com/images/Air-Jordan-1-Retro-High-Dark-Mocha-2-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1616187367",
-            descripcion: "", 
-            },
-            {
-                marca:"addidas",
-                modelo: "Forums",
-                color:"azules",
-                imagen: "https://assets.adidas.com/images/w_600,f_auto,q_auto/43f112d12fea4c098b8aad8a00ebadda_9366/FORUM_84_HI_MARVEL_Blanco_GW5451_01_standard.jpg",
-                descripcion: "", 
-                },
-                {
-                    marca:"addidas",
-                    modelo: "yeezy",
-                    color:"celeste",
-                    imagen: "https://www.fayerwayer.com/resizer/V2yOsamN3FBd_q59773g7kmyTTI=/arc-photo-metroworldnews/arc2-prod/public/EQ5RKHL2UBEO7MSNKDBG64VPXY.jpg",
-                    descripcion: "", 
-                    },
-                    {
-                        marca:"nike",
-                        modelo: "Jordan 11",
-                        color:"negra",
-                        imagen: "https://tecalzoshoes.com/wp-content/uploads/2020/06/JORDAN--e1592306798947.png",
-                        descripcion: "", 
-                        },
-                        {
-                            marca:"addidas",
-                            modelo: "yeezy",
-                            color:"black ",
-                            imagen: "https://images.stockx.com/images/adidas-Yeezy-Boost-350-V2-Black-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607647321",
-                            descripcion: "", 
-                            }
-    
+            color: "rojo",
+            imagen: "images/jordanChicago.webp",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "nike",
+            modelo: "Jordan 1",
+            color: "blanco",
+            imagen: "images/jordanWhite.jpeg",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "nike",
+            modelo: "Jordan 1",
+            color: "university",
+            imagen: "images/jordanUniversityBlue.webp",
+            descripcion: "Las más populares",
+        },
 
-    ]}
+        {
+            marca: "nike",
+            modelo: "Jordan 1",
+            color: "blue",
+            imagen: "images/jordanDarkBlue.webp",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "nike",
+            modelo: "Jordan 1",
+            color: "mocha",
+            imagen: "images/jordanMocha.webp",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "addidas",
+            modelo: "Forums",
+            color: "azules",
+            imagen: "images/adidasWhiteBlue.jpeg",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "addidas",
+            modelo: "yeezy",
+            color: "celeste",
+            imagen: "images/yeezyBlue.jpeg",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "nike",
+            modelo: "Jordan 11",
+            color: "negra",
+            imagen: "images/jordanBlack.png",
+            descripcion: "Las más populares",
+        },
+        {
+            marca: "addidas",
+            modelo: "yeezy",
+            color: "black ",
+            imagen: "images/yeezyBlack.webp",
+            descripcion: "Las más populares",
+        }
 
 
-module.exports= zapatillas
+    ]
+}
+
+
+module.exports = zapatillas
