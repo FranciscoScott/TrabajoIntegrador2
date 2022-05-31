cd84cd3d0cdbd4d059d479c5060fddcca79ffbac
FranciscoScott <fscott@udesa.edu.ar>
2022-05-31 14:42:03 -0300

    modelos de comment y user

---
+++ miProyecto/dataBase/models/Comment.js
@@ -0,0 +1,53 @@
+module.exports = function (sequelize, dataType) {
+
+    //defino el alias. me permite idecntificar el modelo cuando lo usamos en el controlador
+    let alias = "Comment";
+    
+    //Describir la config de las columnas de la tabla
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true, 
+            type: DataTypes.INTEGER
+        },
+        comentario: {
+            type: DataTypes.STRING
+        },
+        userId: {
+            foreignKey: true,
+            type: DataTypes.INTEGER
+        },
+        productId: {
+            foreignKey: true,
+            type: DataTypes.INTEGER
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        },
+        deletedAt: {
+            type: DataTypes.DATE
+        }
+        
+    }
+    
+    let config = {
+        tableName: 'comentarios', //el nombre de la tabla no coincide con el del modelo
+        timestamps: 'true', //columnas para timestamps
+        underscored: 'false' //los campos no tienen guiones bajos
+    }
+    
+    const Comment = sequelize.define (alias, cols, config);
+    
+    
+    // faltan las relaciones
+    
+    
+    
+    
+    return Comment;
+    
+    }
+    
---
+++ miProyecto/dataBase/models/User.js
@@ -0,0 +1,57 @@
+module.exports = function (sequelize, dataType) {
+
+    //defino el alias. me permite idecntificar el modelo cuando lo usamos en el controlador
+    let alias = "User";
+    
+    //Describir la config de las columnas de la tabla
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true, 
+            type: DataTypes.INTEGER
+        },
+        username: {
+            type: DataTypes.STRING
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        bday: {
+            type: DataTypes.DATE
+        },
+        profilePic: {
+            type: DataTypes.STRING
+        },
+        dni: {
+            type: DataTypes.INTEGER
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        },
+        deletedAt: {
+            type: DataTypes.DATE
+        }
+        
+    }
+    
+    let config = {
+        tableName: 'users', //el nombre de la tabla no coincide con el del modelo
+        timestamps: 'true', //columnas para timestamps
+        underscored: 'false' //los campos no tienen guiones bajos
+    }
+    
+    const User = sequelize.define (alias, cols, config);
+    
+    
+    // faltan las relaciones
+    
+    
+    
+    
+    return User;
+    
+    }
+    
