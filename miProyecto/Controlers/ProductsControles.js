238dedb1a387f367bf86d58a8e57d08759fbc4aa
FranciscoScott <fscott@udesa.edu.ar>
2022-06-09 17:53:10 -0300

    ruta y controlador de products

--- miProyecto/Controlers/ProductsControles.js
+++ miProyecto/Controlers/ProductsControles.js
@@ -1,7 +1,87 @@
-const zapatillas = require("../db/Data.js");
+// const zapatillas = require("../db/Data.js");
+
+const db = require ("../database/models/Product");
+
+const Product = db.products
+const op = db.sequelize.Op
+
+const productController = {
+    showProduct: (req, res) => {
+        let id = req.params.id;
+        Product.findByPk (id).then ((results) => {
+            let shoe = {
+                id: result.id,
+                photo: result.photo,
+                model: result.model,
+                description: result.description,
+            }
+            console.log(shoe);
+            return res.render ('products', {products: shoes})
+        }) .catch((err) => {console.log(err);});
+    } , 
+    showProductAdd: function(req, res) {
+        return res.render ('product/add' , {
+            user: database.user, 
+            logueado: database.user.logueado,
+
+        })
+    },
+    store: (req,res) => {
+        let info = req.body;
+        let imgProduct = req.file.fileName;
+        let shoe = {
+            photo: imgProduct, 
+            model: info.model,
+            description: info.description,
+        };
+        Product.create(shoe)
+        .then ((result) => {
+            return res.redirect ('/product')
+        }) .catch((err) => {
+            return res.send ('Hay un error' + err)
+        });
+    },
+    showProductEdit: (req, res) => {
+        let id = req.params.id;
+        Product.findByPk(id).then((result) => {
+            let shoe = {
+                id: result.id,
+                photo: result.photo,
+                model: result.model,
+                description: result.description,
+            }
+            return res.render ('product/edit', {products: shoe})
+        }) .catch((err) => {
+            console.log(err);
+        });
+    },
+    updateProduct: (req, res) => {
+        let info = req.body;
+        let imgProduct = req.file.fileName;
+        let shoe = {
+            photo: imgProduct, 
+            model: info.model,
+            description: info.description,
+        };
+        let filtro = {
+            where: {
+                id: req.params
+            }
+        };
+        console.log(shoe);
+        Product.update(shoe, filtro)
+        .then ((result) => {
+            return res.redirect ('/')
+        }) .catch((err) => {
+            console.log(err);
+        });
+    }
+};
 
 
-const controller = {
+
+
+/* const controller = {
     detail: function (req, res) {
         res.render('product', {
             shoes: zapatillas.shoes,
@@ -20,6 +100,7 @@ const controller = {
     edit: function (req, res) {
         res.render('product')
     },
-}
+} */
+
 //detalle y agregar producto
 module.exports = controller;
--- miProyecto/routes/Product.js
+++ miProyecto/routes/Product.js
@@ -14,4 +14,34 @@ router.get('/edit', ProductsControlers.edit)
 router.get('/:id?', ProductsControlers.detail);
 
 
+// multer para subir fotos
+
+let multer = requiere('multer')
+let path = requiere ('path')
+
+let storage = multer.diskstorage ({
+    destination: function(req, file, cd) {
+        cd (null, path.join (__dirname, '/images')) 
+    },
+    filename: function(req, file, cd) {
+        cd(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    },
+})
+
+let upload = multer({storage : storage})
+
+// para mostrar una zapatilla
+
+router.get('/id/:id', ProductsControlers.showProduct)
 module.exports = router
+
+// para agregar una zapatilla
+
+router.get ('/add', ProductsControlers.showProductAdd)
+
+router.post ('/store', upload.single ('imgProduct'), ProductsControlers.store)
+
+//rutas para editar una zapatilla
+
+router.get ('/edit/:id', ProductsControlers.showProductEdit)
+router.post ('/edited', upload.single ('imgProduct'), ProductsControlers.updateProduct )
