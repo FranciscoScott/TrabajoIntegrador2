337e288872b510603c7f2e01e1be6651a986cb14
santiagochamo <schamo@udesa.edu.ar>
2022-06-14 11:34:15 -0300

    hice la parte de index controllers y corregi la ruta del index

--- miProyecto/Controlers/indexControlers.js
+++ miProyecto/Controlers/indexControlers.js
@@ -1,22 +1,36 @@
 const zapatillas = require("../db/Data.js");
+const db = require ("../database/models")
+const op = db.Sequelize.Op
+const products = db.Product //(o products puede ser el ALIAS)
+
+    const indexController = {
+        index: (req, res ) => {
+            products.findAll({
+                order: [["createdAt" , "DESC"]]
+            })
+            .then ((result) => {
+                return res.render("index" , {products : result})
+            }) .catch((err) => {
+
+            });
+        },
+    }
+ // const controller = {
+    // index: function (req, res) {
+        // res.render('index', {
+           //  shoes: zapatillas.shoes,
+           //  comentarios: zapatillas.comentarios 
+        // });
+   //  },
+
+    // searchresults: function (req, res) {
+       //  res.render('search-results', {
+       //      shoes: zapatillas.shoes,
+        //     comentarios: zapatillas.comentarios
+       //  });
+   //  },
 
 
-const controller = {
-    index: function (req, res) {
-        res.render('index', {
-            shoes: zapatillas.shoes,
-            comentarios: zapatillas.comentarios
-        });
-    },
+// }
 
-    searchresults: function (req, res) {
-        res.render('search-results', {
-            shoes: zapatillas.shoes,
-            comentarios: zapatillas.comentarios
-        });
-    },
-
-
-}
-
-module.exports = controller;
+module.exports = indexController;
--- miProyecto/routes/index.js
+++ miProyecto/routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const indexControlers = require('../Controlers/indexControlers');
-const profileControlers = require('../Controlers/ProfileControles')
+// const profileControlers = require('../Controlers/ProfileControles')
 
 router.get('/',indexControlers.index)
 
-router.get('/login', profileControlers.login)
+//router.get('/login', profileControlers.login)
 
-router.get('/register', profileControlers.register)
+//router.get('/register', profileControlers.register)
 
-router.get('/search-results', indexControlers.searchresults)
+//router.get('/search-results', indexControlers.searchresults)
 
 
 module.exports = router ;
